3a463e525950a5894b9498e253db3424dd7ed03d
Mily <milycrn26@gmail.com>
2019-05-21 11:00:37 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 // if(process.env.NODE_ENV === 'dev'){
     // urlDB = 'mongodb://localhost:27017/cafe';
 // }else{
-    urlDB = 'mongodb+srv://david:NxpaKRz3pkvdhWz@cluster0-zi28h.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 // }
 
 process.env.URLDB = urlDB;
